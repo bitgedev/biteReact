5a099448bf24d5dcbed9e754a00742c0ae752145
Moon Bitgoeun <bitge.dev@gmail.com>
2023-08-07 18:52:54 +0900

    npm 모듈 사용하기

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // console.log("Hello node js");
 
-const calc = require("./calc");
+// const calc = require("./calc");
 
 // console.log(calc);
 
-console.log(calc.add(1, 2));
-console.log(calc.sub(10, 2));
+// console.log(calc.add(1, 2));
+// console.log(calc.sub(10, 2));
+
+const randomColor = require("randomcolor");
+//노드 모듈에 있으면 경로 명시할 필요가 없다.
+
+let color1 = randomColor();
+let color2 = randomColor();
+let color3 = randomColor();
+
+console.log(color1, color2, color3);
